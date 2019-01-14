c218b555653a0d35b3330cf3f4ffe4ee5349d7d6
Kyle Burgi <KyleBurgi@gmail.com>
2019-01-13 17:03:42 -0800

    First Service File
    First service file and directory made. All Models now have test files.

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/MessageTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.TestsModels
+{
+    [TestClass]
+    class MessageTests
+    {
+    }
+}
