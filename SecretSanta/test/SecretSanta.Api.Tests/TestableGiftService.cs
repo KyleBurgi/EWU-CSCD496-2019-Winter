332422711907bafbcacf3eea59de367afc6530ac
Kyle Burgi <KyleBurgi@gmail.com>
2019-01-29 23:44:01 -0800

    Updated one file

--- SecretSanta/test/SecretSanta.Api.Tests/TestableGiftService.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableGiftService.cs
@@ -9,10 +9,25 @@ namespace SecretSanta.Api.Tests
         public List<Gift> ToReturn { get; set; }
         public int GetGiftsForUser_UserId { get; set; }
 
+        public Gift AddGiftToUser(int userId, Gift gift)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public List<Gift> GetGiftsForUser(int userId)
         {
             GetGiftsForUser_UserId = userId;
             return ToReturn;
+        }
+
+        public void RemoveGift(Gift gift)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Gift UpdateGiftForUser(int userId, Gift gift)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
