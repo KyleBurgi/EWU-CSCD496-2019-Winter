0453587acb40378146ab8a29f2ec9127c22287ff
Kyle Burgi <KyleBurgi@gmail.com>
2019-02-10 20:55:07 -0800

    Initial Commit

--- SecretSanta/src/SecretSanta.Domain/Models/Gift.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/Gift.cs
@@ -4,7 +4,7 @@ namespace SecretSanta.Domain.Models
 {
     public class Gift
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         [Required]
         public string Title { get; set; }
         public string Description { get; set; }
