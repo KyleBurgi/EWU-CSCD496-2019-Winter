0d9f738fcc0cffe856705288c904bd6bfeb3609c
Kyle Burgi <KyleBurgi@gmail.com>
2019-02-17 13:23:19 -0800

    GiftService & IGiftService

--- SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SecretSanta.Domain.Models;
 using SecretSanta.Domain.Services.Interfaces;
@@ -16,39 +17,40 @@ namespace SecretSanta.Domain.Services
             DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         }
 
-        public Gift AddGiftToUser(int userId, Gift gift)
+        public async Task<Gift> AddGiftToUser(int userId, Gift gift)
         {
             if (gift == null) throw new ArgumentNullException(nameof(gift));
 
             gift.UserId = userId;
-            DbContext.Gifts.Add(gift);
-            DbContext.SaveChanges();
+            await DbContext.Gifts.AddAsync(gift);
+            await DbContext.SaveChangesAsync();
 
             return gift;
         }
 
-        public Gift UpdateGiftForUser(int userId, Gift gift)
+        public async Task<Gift> UpdateGiftForUser(int userId, Gift gift)
         {
             if (gift == null) throw new ArgumentNullException(nameof(gift));
 
             gift.UserId = userId;
             DbContext.Gifts.Update(gift);
-            DbContext.SaveChanges();
+            await DbContext.SaveChangesAsync();
 
             return gift;
         }
 
-        public List<Gift> GetGiftsForUser(int userId)
+        public async Task<List<Gift>> GetGiftsForUser(int userId)
         {
-            return DbContext.Gifts.Where(g => g.UserId == userId).ToList();
+            return await DbContext.Gifts.Where(g => g.UserId == userId).ToListAsync();
         }
 
-        public void RemoveGift(Gift gift)
+        public async Task<bool> RemoveGift(Gift gift)
         {
             if (gift == null) throw new ArgumentNullException(nameof(gift));
 
             DbContext.Gifts.Remove(gift);
-            DbContext.SaveChanges();
+            await DbContext.SaveChangesAsync();
+            return true;
         }
     }
 }
